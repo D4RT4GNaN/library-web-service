852db5d887a64a7d8d8d670849779a51dad2b9a0
D4RT4GNaN <maxime.blaise06@gmail.com>
2019-10-31 15:06:13 +0100

    Generated code with WSDL and config the web service for Tomcat Server

---
+++ libraryservice-webservice/src/main/java/generated/libraryservice/GetListBook.java
@@ -0,0 +1,148 @@
+
+package generated.libraryservice;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for anonymous complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="keyword" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="author" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="publisher" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *         &lt;element name="language" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "keyword",
+    "author",
+    "publisher",
+    "language"
+})
+@XmlRootElement(name = "getListBook")
+public class GetListBook {
+
+    @XmlElement(required = true)
+    protected String keyword;
+    @XmlElement(required = true)
+    protected String author;
+    @XmlElement(required = true)
+    protected String publisher;
+    @XmlElement(required = true)
+    protected String language;
+
+    /**
+     * Gets the value of the keyword property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getKeyword() {
+        return keyword;
+    }
+
+    /**
+     * Sets the value of the keyword property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setKeyword(String value) {
+        this.keyword = value;
+    }
+
+    /**
+     * Gets the value of the author property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * Sets the value of the author property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAuthor(String value) {
+        this.author = value;
+    }
+
+    /**
+     * Gets the value of the publisher property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPublisher() {
+        return publisher;
+    }
+
+    /**
+     * Sets the value of the publisher property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPublisher(String value) {
+        this.publisher = value;
+    }
+
+    /**
+     * Gets the value of the language property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getLanguage() {
+        return language;
+    }
+
+    /**
+     * Sets the value of the language property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setLanguage(String value) {
+        this.language = value;
+    }
+
+}
---
+++ libraryservice-webservice/src/main/java/libraryservice/LibraryService.java
@@ -0,0 +1,52 @@
+package libraryservice;
+
+import generated.libraryservice.Book;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.List;
+
+@WebService(endpointInterface = "generated.libraryservice.LibraryService")
+public class LibraryService  implements generated.libraryservice.LibraryService {
+    @WebMethod
+    public int getBookAvailability(String bookReference) {
+        return 0;
+    }
+
+    @WebMethod
+    public String addNewLoan(XMLGregorianCalendar borrowingDate, String bookReference, int userID) {
+        return null;
+    }
+
+    @WebMethod
+    public boolean extendLoan(XMLGregorianCalendar borrowingDate, String bookReference, int userID) {
+        return false;
+    }
+
+    @WebMethod
+    public String returnBook(XMLGregorianCalendar borrowingDate, String bookReference, int userID) {
+        return null;
+    }
+
+    @WebMethod
+    public String getStatusLoan(XMLGregorianCalendar borrowingDate, String bookReference, int userID) {
+        return null;
+    }
+
+    @WebMethod
+    public List<Book> getListBook(String keyword, String author, String publisher, String language) {
+        return null;
+    }
+
+    @WebMethod
+    public String addUser(String username, String password, String firstname, String lastname, String mail, String adress) {
+        return "Test : " + username;
+    }
+
+    @WebMethod
+    public String connectUser(String identifier, String password) {
+        return null;
+    }
+}
